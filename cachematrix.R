55412a243b3904d7c60aba3307b5936ef1f12e72
sauhardawasthi <74519745+sauhardawasthi@users.noreply.github.com>
2020-11-16 08:39:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,14 @@
 
 ## My R programming course Assignemnt 2
 
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) { ## defining the argument with the default mode of matrix
     inv <- NULL                             ## initializing inv as NULL as it'll hold matrix inverse's value
     set <- function(y) {                    ## defining the set function to assign new 
         x <<- y                             ## matrix value in parent environment
         inv <<- NULL                        ## if there is a new matrix, resetting inv to NULL
     }
-    get <- function() x                     ## defining the get fucntion
+    get <- function() x                     ## defining the get function here
 
     setinverse <- function(inverse) inv <<- inverse  ## assigning inv's value in parent environment
     getinverse <- function() inv                     ## getting inv's value where called
@@ -17,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) { ## defining the argument with the de
 
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
-
+## If the inverse has already been calculated (and matrix hasn't changed), then cacheSolve retrieves innverse from cache.
 cacheSolve <- function(x, ...) {
         ## Returning matrix that's inverse of 'x'
     inv <- x$getinverse()
